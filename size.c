e7c2bf1de43d48bb36cf5cf4f10c201f046a6426
Ashitesh1403 <69584623+Ashitesh1403@users.noreply.github.com>
2020-08-12 17:45:26 +0530

    To find size of the required variabes

---
+++ size.c
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int integerType;
+ float floatType;
+ char characterType;
+ double doubleType;
+// where cout is to calculate and print
+// The size of integer type
+ cout<<"size of int is:"<<
+ sizeof(integerType)<<"\n";
+
+ // where cout is to calculate and print.
+ // The size of integer type.
+ cout<<"size of char is:"<<
+ sizeof(characterType)<<"\n";
+
+ //where cout is to calculate and print.
+ // The size of integer type
+ cout<<"size of float is:"<<
+ sizeof(floatType)<<"\n";
+
+ // where cout is to calculate and print.
+ // The size of integer type
+ cout<<"size of Double is:"<<
+ sizeof(doubleType)<<"\n";
+
+ return 0;
+}
