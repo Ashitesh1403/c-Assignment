3881ca8de8ddb15b83eadcf7d6a2c5c96ef80c32
Ashitesh1403 <69584623+Ashitesh1403@users.noreply.github.com>
2020-08-30 16:16:11 +0530

    student dbms.c

---
+++ student dbms.c
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <conio.h>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+    FILE *fp, *ft;
+    char another, choice;
+
+    struct student
+    {
+        char first_name[50], last_name[50];
+        char course[100];
+        int section;
+    };
+
+    struct student e;
+    char xfirst_name[50], xlast_name[50];
+    long int recsize;
+
+    fp=fopen("users.txt","rb+");
+
+    if (fp == NULL)
+    {
+        fp = fopen("users.txt","wb+");
+
+        if (fp==NULL)
+        {
+            puts("Cannot open file");
+            return 0;
+        }
+    }
